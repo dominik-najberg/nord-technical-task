23cdac69756421b027592cea1837d3983ab4d44d
Dominik Najberg <dominik.najberg@gmail.com>
2023-09-15 08:03:44 +0200

    Doctrine Cache

--- src/Application/Geocoder/Geocoder.php
+++ src/Application/Geocoder/Geocoder.php
@@ -2,18 +2,23 @@
 
 namespace App\Application\Geocoder;
 
+use App\Application\Address\ResolvedAddress;
 use App\Application\Address\ValueObject\Address;
 use App\Application\Address\ValueObject\Coordinates;
+use App\Application\Repository\ResolvedAddressRepository;
+use App\Infrastructure\Repository\DoctrineResolvedAddressRepository;
 
 class Geocoder
 {
     /** @var GeocoderClient[] */
     private array $geocoders;
+    private ResolvedAddressRepository $resolvedAddressRepository;
 
-    public function __construct(array $geocoders)
+    public function __construct(array $geocoders, ResolvedAddressRepository $resolvedAddressRepository)
     {
         array_map(static fn (GeocoderClient $geocoder) => $geocoder, $geocoders);
         $this->geocoders = $geocoders;
+        $this->resolvedAddressRepository = $resolvedAddressRepository;
     }
 
     public function geocode(Address $address): ?Coordinates
@@ -21,6 +26,8 @@ class Geocoder
         foreach ($this->geocoders as $geocoder) {
             $coordinates = $geocoder->geocode($address);
             if (null !== $coordinates) {
+                $this->resolvedAddressRepository->saveResolvedAddress($address, $coordinates);
+
                 return $coordinates;
             }
         }
